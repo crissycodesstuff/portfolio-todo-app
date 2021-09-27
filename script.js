89808e584cacf00c820c4964895d657826871053
Glitch (glitch-hello-website) <none>
2021-09-27 17:59:56 +0000

    🗽🏐 Checkpoint ./script.js:36554723/512 ./index.html:36554723/140

--- script.js
+++ script.js
@@ -2,4 +2,34 @@ const taskInput = document.getElementById("new");
 const addButton = document.getElementById("add");
 const tasks = document.getElementById("tasks");
 
-console.log(taskInput, addButton, tasks)
+console.log(taskInput, addButton, tasks);
+
+addButtom.addEventListener("click, addNewItem");
+
+const items = [
+  { value: "feed pickle", 
+    complete: false
+  },
+  { value: "feed pickle", 
+    complete: false
+  },
+  { value: "feed pickle", 
+    complete: false
+  },
+  { value: "feed pickle", 
+    complete: false
+  },
+  
+];
+
+for(let item of items) {
+  createElementForTask(item);
+}
+
+function createElementForTask(item) {
+  
+}
+
+function addNewItem() {
+  console.log("you clicked the button");
+}
