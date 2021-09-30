a6e6a77d0d0053dd59b6e93d373b90d8f6bcc2f3
Glitch (glitch-hello-website) <none>
2021-09-30 18:31:28 +0000

    🏥🌸 Checkpoint ./script.js:36554723/7146

--- script.js
+++ script.js
@@ -1,73 +1,76 @@
-const taskInput = document.getElementById("new");
-const addButton = document.getElementById("add");
-const tasks = document.getElementById("tasks");
+(async function () {
+  
+  const taskInput = document.getElementById("new");
+  const addButton = document.getElementById("add");
+  const tasks = document.getElementById("tasks");
 
-addButton.addEventListener("click", addNewItem);
-taskInput.addEventListener("keyup", processKeyPress);
+  addButton.addEventListener("click", addNewItem);
+  taskInput.addEventListener("keyup", processKeyPress);
 
-const items = getItems();
+  const items = getItems();
 
-for (let item of items) {
-  const li = createElementForTask(item);
-  tasks.appendChild(li);
-}
-
-function processKeyPress(event) {
-  addButton.disabled = event.target.value.trim() === "";
-
-  if (event.key === "Enter" && !addButton.disabled) {
-    addNewItem();
+  for (let item of items) {
+    const li = createElementForTask(item);
+    tasks.appendChild(li);
   }
-}
 
-function createElementForTask(item) {
-  const template = document.getElementById("taskTemplate");
-  const newListItem = template.content.cloneNode(true);
+  function processKeyPress(event) {
+    addButton.disabled = event.target.value.trim() === "";
 
-  const checkbox = newListItem.querySelector(".item-check");
-  const text = newListItem.querySelector(".item-text");
-  const deleteButton = newListItem.querySelector(".delete");
+    if (event.key === "Enter" && !addButton.disabled) {
+      addNewItem();
+    }
+  }
 
-  text.innerText = item.value;
-  checkbox.checked = item.complete;
+  function createElementForTask(item) {
+    const template = document.getElementById("taskTemplate");
+    const newListItem = template.content.cloneNode(true);
 
-  deleteButton.onclick = function(event) {
-    event.target.closest("li").remove();
-    items.splice(items.indexOf(item), 1);
+    const checkbox = newListItem.querySelector(".item-check");
+    const text = newListItem.querySelector(".item-text");
+    const deleteButton = newListItem.querySelector(".delete");
+
+    text.innerText = item.value;
+    checkbox.checked = item.complete;
+
+    deleteButton.onclick = function(event) {
+      event.target.closest("li").remove();
+      items.splice(items.indexOf(item), 1);
+      saveItems();
+    };
+
+    checkbox.onchange = function(event) {
+      item.complete = event.target.checked;
+      saveItems();
+    };
+
+    return newListItem;
+  }
+
+  function addNewItem() {
+    const task = {
+      value: taskInput.value,
+      complete: false
+    };
+
+    items.push(task);
     saveItems();
-  };
 
-  checkbox.onchange = function(event) {
-    item.complete = event.target.checked;
-    saveItems();
-  };
+    let newItem = createElementForTask(task);
+    tasks.appendChild(newItem);
 
-  return newListItem;
-}
+    taskInput.value = "";
+    taskInput.focus();
+  }
 
-function addNewItem() {
-  const task = {
-    value: taskInput.value,
-    complete: false
-  };
+  function getItems() {
+    const noItemsFound = "[]";
+    const itemsJSON = localStorage.getItem("items") || noItemsFound;
+    return JSON.parse(itemsJSON);
+  }
 
-  items.push(task);
-  saveItems();
-
-  let newItem = createElementForTask(task);
-  tasks.appendChild(newItem);
-
-  taskInput.value = "";
-  taskInput.focus();
-}
-
-function getItems() {
-  const noItemsFound = "[]";
-  const itemsJSON = localStorage.getItem("items") || noItemsFound;
-  return JSON.parse(itemsJSON);
-}
-
-function saveItems() {
-  const data = JSON.stringify(items);
-  localStorage.setItem("items", data);
-}
+  function saveItems() {
+    const data = JSON.stringify(items);
+    localStorage.setItem("items", data);
+  }
+})();
