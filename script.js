ef4de1b72e15f05985c87b118d04a3d21f44c270
Glitch (glitch-hello-website) <none>
2021-09-30 18:19:37 +0000

    🏚🏫 Checkpoint ./script.js:36554723/6685

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ const addButton = document.getElementById("add");
 const tasks = document.getElementById("tasks");
 
 addButton.addEventListener("click", addNewItem);
-taskInput.addEventListener("keyup", processKeyPress)
+taskInput.addEventListener("keyup", processKeyPress);
 
-const items = [];
+const items = getItems();
 
 for (let item of items) {
   const li = createElementForTask(item);
@@ -14,7 +14,7 @@ for (let item of items) {
 
 function processKeyPress(event) {
   addButton.disabled = event.target.value.trim() === "";
-  
+
   if (event.key === "Enter" && !addButton.disabled) {
     addNewItem();
   }
@@ -23,23 +23,25 @@ function processKeyPress(event) {
 function createElementForTask(item) {
   const template = document.getElementById("taskTemplate");
   const newListItem = template.content.cloneNode(true);
-  
+
   const checkbox = newListItem.querySelector(".item-check");
   const text = newListItem.querySelector(".item-text");
   const deleteButton = newListItem.querySelector(".delete");
-  
+
   text.innerText = item.value;
   checkbox.checked = item.complete;
-  
-  deleteButton.onclick = function (event) {
-    event.target.closest('li').remove();
+
+  deleteButton.onclick = function(event) {
+    event.target.closest("li").remove();
     items.splice(items.indexOf(item), 1);
-  }
-  
-  checkbox.onchange = function (event) {
+    saveItems();
+  };
+
+  checkbox.onchange = function(event) {
     item.complete = event.target.checked;
-  }
-  
+    saveItems();
+  };
+
   return newListItem;
 }
 
@@ -48,13 +50,24 @@ function addNewItem() {
     value: taskInput.value,
     complete: false
   };
-  
+
   items.push(task);
-  
+  saveItems();
+
   let newItem = createElementForTask(task);
   tasks.appendChild(newItem);
-  
+
   taskInput.value = "";
   taskInput.focus();
 }
 
+function getItems() {
+  const noItemsFound = "[]";
+  const itemsJSON = localStorage.getItem("items") || noItemsFound;
+  return JSON.parse(itemsJSON);
+}
+
+function saveItems() {
+  const data = JSON.stringify(items);
+  localStorage.setItem("items", data);
+}
